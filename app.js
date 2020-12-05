63ea3868421d7179a06b58bcfc7f6864f7c7e931
David Reband <david.reband@webanizer.de>
2020-12-05 21:11:16 +0100

    completed

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-export default (express) => {
+export default (express, bodyParser, createReadStream, crypto, http) => {
   const CORS = {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "GET,POST,PUT,PATCH,OPTIONS,DELETE",
@@ -12,49 +12,41 @@ export default (express) => {
       next();
     })
     .get("/login/", (req, res) => res.send("davidreband"))
-    .all("*", (req, res) => {
-      res
-        .status(404)
-        .set({"Content-Type": "text/html; charset=utf-8", ...CORS})
-        .send("davidreband");
-    })
-    //.use((error, req, res, next) => res.status(500).set(CORS).send("Error"));
-
-  /*
-    .use((req, res, next) => {
-      res.set(CORS);
-      next();
-    })
-    .get("/login/", (req, res) => res.send("poli8512"))
     .get("/code/", (req, res) => {
       res.set({ "Content-Type": "text/plain; charset=utf-8" });
       createReadStream(import.meta.url.substring(7)).pipe(res);
     })
     .get("/sha1/:input", (req, res) => {
-      const param = req.params;
+      const { input } = req.params;
       const shasum = crypto.createHash("sha1");
-      shasum.update(param);
+      shasum.update(input);
       res.send(shasum.digest("hex"));
     })
-    .all("/req/", (req, res) => {
-      let url = req.method === "POST" ? req.body.addr : req.query.addr;
-
-      http.get(url, (response) => {
-        let data = "";
-        response.on("data", (chunk) => (data += chunk));
-        response.on("end", () => {
-          res
+    .all('/req/', (req, res) => {
+        let url = req.method === 'POST' ? req.body.addr : req.query.addr;
+        http.get(url, (response) => {
+        let data = '';
+        response.on('data', (chunk) => (data += chunk));
+        response.on('end', () => {
+            res
             .set({
-              "Content-Type": "text/plain; charset=utf-8",
+                'Content-Type': 'text/plain; charset=utf-8',
             })
             .end(data);
         });
-      });
+        });
     })
     .all("*", (req, res) => {
-      res.send("poli8512");
+      res
+        .status(404)
+        .set({ "Content-Type": "text/html; charset=utf-8" })
+        .send("davidreband");
     })
-    .use((error, req, res, next) => res.status(500).set(CORS).send("Error"));
-    */
+    .use((error, req, res, next) => {
+      res
+        .status(500)
+        .set({ "Content-Type": "text/html; charset=utf-8" })
+        .send("Error");
+    });
   return app;
 };
--- index.js
+++ index.js
@@ -3,15 +3,9 @@ import bodyParser from 'body-parser'
 import { createReadStream } from 'fs'
 import crypto from "crypto";
 import http from "http";
-import expressApp from "./app.js";
+import appSrc from "./app.js";
 
-const app = expressApp(
-  express ,
-  bodyParser,
-  createReadStream,
-  crypto,
-  http
-);
+const app = appSrc(express, bodyParser, createReadStream, crypto, http);
 
 try {
   app.listen(process.env.PORT ?? 4321);
