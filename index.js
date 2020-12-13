49b26d2f7687699b3f48a98b651fba3c00a8f7d9
David Reband <david.reband@webanizer.de>
2020-12-13 01:29:51 +0100

    routes user

--- index.js
+++ index.js
@@ -5,14 +5,17 @@ import mongoose from 'mongoose'
 import crypto from "crypto"
 import http from "http"
 import appSrc from "./app.js"
-import dot  from 'dotenv'
+//import dot  from 'dotenv'
 import CORS from "./CORS.js"
 import UserModel from "./models/User.js"
 import UserController from "./routes/UserController.js"
 
 
-dot.config({ path: "./.env" })
-const { URL } = process.env
+//dot.config({ path: "./.env" })
+//const { URL } = process.env
+
+const URL = "mongodb+srv://davidreband:umGN4stz3622@cluster0.gviiz.mongodb.net/mongodemo?retryWrites=true&w=majority"
+
 const User = UserModel(mongoose);
 const app = appSrc(express, bodyParser, createReadStream, crypto, http, mongoose, User, UserController, CORS)
 
