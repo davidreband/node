dcf8a47a58755c45e6e9fe00eb543bec549f56cf
David Reband <david.reband@webanizer.de>
2020-12-26 22:19:42 +0100

    options

--- CORS.js
+++ CORS.js
@@ -1,9 +1,10 @@
 const CORS = {
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS",
+  "Access-Control-Expose-Headers": "author, Access-Control-Expose-Headers",
   "Access-Control-Allow-Headers":
     "x-test,Content-Type,Accept, Access-Control-Allow-Headers",
-    "author": "davidreband"
+  author: "davidreband",
 };
 
 export default CORS;
